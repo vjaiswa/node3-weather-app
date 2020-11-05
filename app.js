e8c80c2379adc5759ea4d91d9c601235db4c2c40
Vijay Jaiswal <jais_myvijay@yahoo.co.in>
2020-11-05 15:39:42 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const geoCodeObject = require('./utils/geocode')
+const forecastObject = require('./utils/forecast.js')
+const command = process.argv[2]
+if(!command){
+    console.log('Please provide location.')
+}else{
+    geoCodeObject.geoCode(command,(error,{latitude,longitude,place}={}) =>{
+        if(error){
+            console.log(error)
+        }else{
+            forecastObject.forecast(latitude,longitude,(error,{temperature,feelsLike,description}) =>{
+                if(error){
+                    console.log(error)
+                }else{
+                    console.log(latitude)
+                    console.log(longitude)
+                    console.log(place)
+                    console.log(temperature)
+                    console.log(feelsLike)
+                    console.log(description)
+                }
+            })        
+        }
+    })
+}
+
+
+
